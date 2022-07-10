d083266e0412865a1336b879c71c78a3fb567a1c
Joshua Pocsidio <joshua.pocsidio@gmail.com>
2022-07-10 21:29:50 +0800

    Revision A - Added unversioned files

---
+++ lib/model/DataControllerManager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_windows/model/game/GameDataController.dart';
+
+class DataControllerManager {
+  final GameDataController gameDataController;
+
+  DataControllerManager({required this.gameDataController});
+}
